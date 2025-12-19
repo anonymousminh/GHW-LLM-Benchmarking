b258da2da47c3432f2c1caea0da6774536cc69e6
Minh <anhminh7802@gmail.com>
2025-12-18 22:56:55 -0700

    Refactor API key handling and client initialization in home.py

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from google import genai
 import time
+import os
 import pandas as pd
 import plotly.express as px
 from groq import Groq
@@ -12,7 +13,26 @@ st.subheader("Compare LLMs side by side")
 st.divider()
 
 # API Key for the LLMs
-gemini_client = genai.Client(api_key="")
-groq_client = Groq(api_key="")
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+
+gemini_client = genai.Client(GOOGLE_API_KEY)
+groq_client = Groq(GROQ_API_KEY)
+
+def call_gemini(prompt):
+    start = time.time()
+    response = gemini_client.models.generate_content(model='gemini-3-flash', 
+                                                     contents=prompt)
+    
+    end = time.time()
+    if response.usage_metadata:
+        token_count = response.usage_metadata.total_token_count
+    else:
+        token_count = len(response.text) // 4
+
+    return response.text, end - start, token_count
+
+
+
 
 
